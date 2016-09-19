2de73c519a8ff0b9005d7faf688e6afe8b753cf9
darq <darq@PC-DARQ>
2016-09-19 23:37:09 +0200

    The main class

---
+++ RepTest/src/org/qteam/reptest/RepTest.java
@@ -0,0 +1,17 @@
+package org.qteam.reptest;
+
+/**
+	Simple github test.
+	@author darq
+*/
+public class RepTest
+{
+	/**
+		The main class.
+		@param args The command line arguments.
+	*/
+    public static void main (String args [])
+	{
+		System.out.println ("Test !!!");
+    }
+}
